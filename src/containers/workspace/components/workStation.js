cd8e23f5095f4690133a8f866a9cbb7210fb2f6a
Chenglu Li <kabelee92@gmail.com>
2018-08-25 16:24:27 -0500

    Formatting

--- src/containers/workspace/components/workStation.js
+++ src/containers/workspace/components/workStation.js
@@ -1,66 +1,66 @@
 // Cannot use typescript because of a lack of typing
-import "draft-js-inline-toolbar-plugin/lib/plugin.css";
-import * as React from "react";
-import { Input } from "antd";
-import Editor, { createEditorStateWithText } from "draft-js-plugins-editor";
-import styled from "styled-components";
+import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
+import React from 'react';
+import { Input } from 'antd';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import styled from 'styled-components';
 
-import { Colors } from "~/themes";
+import { Colors } from '~/themes';
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: flex-start;
-	height: calc(100vh - 24px - 24px - 64px);
-	&&&& > .DraftEditor-root {
-		padding-right: 1rem;
-		flex: 0.8;
-		max-height: 100%;
-		overflow-y: auto;
-	}
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: calc(100vh - 24px - 24px - 64px);
+  &&&& > .DraftEditor-root {
+    padding-right: 1rem;
+    flex: 0.8;
+    max-height: 100%;
+    overflow-y: auto;
+  }
 `;
 
 const SideContainer = styled.div`
-	flex: 0.2;
-	padding: 1rem 0 0 1rem;
-	border-left: solid 1px ${Colors.borderGray.toString()};
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
-	align-items: center;
-	height: 100%;
+  flex: 0.2;
+  padding: 1rem 0 0 1rem;
+  border-left: solid 1px ${Colors.borderGray.toString()};
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100%;
 `;
 
 const text =
-	"In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … ";
+  'In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … In this editor a toolbar shows up once you select part of the text … ';
 
 export default class WorkStation extends React.Component {
-	state = {
-		editorState: createEditorStateWithText(text)
-	};
+  state = {
+    editorState: createEditorStateWithText(text),
+  };
 
-	onChange = editorState => {
-		this.setState({
-			editorState
-		});
-	};
+  onChange = editorState => {
+    this.setState({
+      editorState,
+    });
+  };
 
-	render() {
-		return (
-			<Container>
-				<Editor
-					editorState={this.state.editorState}
-					onChange={this.onChange}
-					ref={element => {
-						this.editor = element;
-					}}
-				/>
+  render() {
+    return (
+      <Container>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          ref={element => {
+            this.editor = element;
+          }}
+        />
 
-				<SideContainer>
-					<Input />
-				</SideContainer>
-			</Container>
-		);
-	}
+        <SideContainer>
+          <Input />
+        </SideContainer>
+      </Container>
+    );
+  }
 }
