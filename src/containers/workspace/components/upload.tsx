3578ce14da1e027dd2de6102ca468dc3300ec28c
Chenglu Li <kabelee92@gmail.com>
2018-10-06 16:39:54 -0500

    Update manifest file

--- src/containers/workspace/components/upload.tsx
+++ src/containers/workspace/components/upload.tsx
@@ -1,21 +1,21 @@
-import {Icon, message, Upload} from 'antd'
-import React from 'react'
-import styled from 'styled-components'
+import { Icon, message, Upload } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
 
-const Dragger = Upload.Dragger
+const Dragger = Upload.Dragger;
 
 function getBase64(text: Blob, callback: (res: FileReader['result']) => void) {
-  const reader = new FileReader()
-  reader.addEventListener('load', () => callback(reader.result))
-  reader.readAsText(text, 'UTF-8')
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsText(text, 'UTF-8');
 }
 
 interface UploaderProps {
-  onCompleteUpload: (data: {text: string; name: string}) => void
+  onCompleteUpload: (data: { text: string; name: string }) => void;
 }
 
 interface UploaderState {
-  loading: boolean
+  loading: boolean;
 }
 
 const StyledDragger = styled(Dragger)`
@@ -34,33 +34,36 @@ const StyledDragger = styled(Dragger)`
       font-size: 8rem;
     }
   }
-`
+`;
 
-export default class Uploader extends React.PureComponent<UploaderProps, UploaderState> {
+export default class Uploader extends React.PureComponent<
+  UploaderProps,
+  UploaderState
+> {
   public state = {
-    loading: false
-  }
+    loading: false,
+  };
 
   public handleChange = (info: AntUploadChangeParam) => {
     if (info.file.status === 'uploading') {
-      this.setState({loading: true})
-      return
+      this.setState({ loading: true });
+      return;
     }
     if (info.file.status === 'done' && info.file.originFileObj) {
       // Get this url from response in real world.
       getBase64(info.file.originFileObj, (text: string) => {
         this.setState({
-          loading: false
-        })
+          loading: false,
+        });
         this.props.onCompleteUpload({
           text,
-          name: info.file.originFileObj ? info.file.originFileObj.name : 'N/A'
-        })
-      })
+          name: info.file.originFileObj ? info.file.originFileObj.name : 'N/A',
+        });
+      });
     } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`)
+      message.error(`${info.file.name} file upload failed.`);
     }
-  }
+  };
 
   public render() {
     return (
@@ -72,11 +75,13 @@ export default class Uploader extends React.PureComponent<UploaderProps, Uploade
         <p className="ant-upload-drag-icon">
           <Icon type="inbox" />
         </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
         <p className="ant-upload-hint">
           Currently, the app only accepts text file with extension of ".txt"
         </p>
       </StyledDragger>
-    )
+    );
   }
 }
