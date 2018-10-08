c58664f3451693e9493a7d0f43ceebda84e7cbb3
Li, Chenglu <kabelee92@gmail.com>
2018-10-08 09:29:30 -0500

    Use shell script for deployment...

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+const shell = require('shelljs');
+
+if (shell.exec('npm run build').code !== 0) {
+  shell.echo('Error: Failed to build');
+  shell.exit(1);
+}
+
+if (shell.exec('git subtree push --prefix dist heroku master').code !== 0) {
+  shell.echo('Error: Failed to deploy build folder to heroku');
+  shell.exit(1);
+}
