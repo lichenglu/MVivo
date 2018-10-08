4f1f7d3b473e51d7556ab877fe2b82b61e425156
Chenglu Li <kabelee92@gmail.com>
2018-10-08 17:28:53 -0500

    New build

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,16 @@ const shell = require('shelljs');
 
 shell.echo('Building starts...');
 
-if (shell.exec('npm run build:github').code !== 0) {
+if (shell.exec('npm run build').code !== 0) {
   shell.echo('Error: Failed to build');
   shell.exit(1);
 }
 
-if (shell.exec('git subtree push --prefix dist origin gh-pages').code !== 0) {
+shell.exec(
+  `echo "<?php header( 'Location: /index.html' ) ;  ?>" > ./dist/index.php`
+);
+
+if (shell.exec('git subtree push --prefix dist heroku master').code !== 0) {
   shell.echo('Error: Failed to deploy build folder to heroku');
   shell.exit(1);
 }
